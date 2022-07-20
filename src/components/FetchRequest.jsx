7265afa9cd9410999d89e64fb17209a9840bc8d6
Anusha Singla <anushasingla737@gmail.com>
2022-07-20 19:07:30 +0530

    useFetch

--- src/components/FetchRequest.jsx
+++ src/components/FetchRequest.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from "react";
-import axios from 'axios';
+// import { useState } from "react";
+import useFetch from "../customHooks/useFetch";
 
 const FetchRequest = () => {
-    const [advice, setAdvice] = useState('');
-    const fetchApi = async() => {
-        const response = await fetch('https://api.adviceslip.com/advice');   
-        const data = await response.json();
-        setAdvice(data.slip.advice)
-    }
-    useEffect(() => {
-        fetchApi();
-    }, [])
+    // const [advice, setAdvice] = useState('');
+    // const fetchApi = async() => {
+    //     const response = await fetch('https://api.adviceslip.com/advice');   
+    //     const data = await response.json();
+    //     setAdvice(data.slip.advice)
+    // }
+    // useEffect(() => {
+    //     fetchApi();
+    // }, [])
 
+    const [data] = useFetch('https://api.adviceslip.com/advice')
     return (
         <div className="app">
             <div className="card">
-                <h1 className="heading">{advice}</h1>
-                <button className="button" onClick={fetchApi}>
+                <h1 className="heading">{data?.slip.advice}</h1> {/*Random right now*/}
+                <button className="button">
                     <span>Give me Advice</span>
                 </button>
             </div>
